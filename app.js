26e33a2c95870452c118bd9d8a6b980f030d1198
DEVNETWORLD <94292739+DEVNETWORLD@users.noreply.github.com>
2023-01-25 01:35:47 +0530

    second alert message

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var btnTranslate = document.querySelector("#btn-trans");
-var textInput = document.querySelector("#txt-input");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(input){
+  return serverURL + "?"+ "text=" + input;
+}
+function errorHandler(error){
+  console.log("Error has occured!", error);
+  alert("there has been an error!!!");
+}
 function clickEventHandler()
 {
-  outputDiv.innerText= textInput.value;
+  var inputText = txtInput.value;
+  console.log(inputText);
+  if(inputText== 0){
+    alert("please write a text");
+  }
+  else{
+   fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => { var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;})
+    .catch(errorHandler);}
 }
 
-btnTranslate.addEventListener("click",clickEventHandler)
 
+btnTranslate.addEventListener("click",clickEventHandler)
